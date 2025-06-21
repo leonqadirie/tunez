390b14077831ce83539a127d359dc519466cc175
leonqadirie <leon.qadirie@nulltree.xyz>
2025-06-21 20:32:59 +0200

    chore(deps): add ash_postgres

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Tunez.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ash_postgres, "~> 2.0"},
       {:sourceror, "~> 1.8", only: [:dev, :test]},
       {:ash, "~> 3.0"},
       {:phoenix, "~> 1.8.0-rc.1", override: true},
