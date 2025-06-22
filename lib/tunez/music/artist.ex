98e25bd6ba3e0110af5f103b2691c31dec42f2b7
Leon Qadirie <leon.qadirie@nulltree.xyz>
2025-06-22 23:00:10 +0200

    feat: add search action to Artist

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -13,6 +13,15 @@ defmodule Tunez.Music.Artist do
       accept [:name, :biography]
     end
 
+    read :search do
+      argument :query, :ci_string do
+        constraints allow_empty?: true
+        default ""
+      end
+
+      filter expr(contains(name, ^arg(:query)))
+    end
+
     update :update do
       require_atomic? false
       accept [:name, :biography]
