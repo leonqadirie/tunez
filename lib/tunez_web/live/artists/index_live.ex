031e0bbde1dd3af7316b6e8b0fc78d3418522be4
Leon Qadirie <leon.qadirie@nulltree.xyz>
2025-07-06 22:58:32 +0200

    feat: identify actor when calling actions

--- lib/tunez_web/live/artists/index_live.ex
+++ lib/tunez_web/live/artists/index_live.ex
@@ -20,7 +20,8 @@ defmodule TunezWeb.Artists.IndexLive do
       Tunez.Music.search_artists!(
         query_text,
         page: page_params,
-        query: [sort_input: sort_by]
+        query: [sort_input: sort_by],
+        actor: socket.assigns.current_user
       )
 
     socket =
--- lib/tunez_web/live/artists/show_live.ex
+++ lib/tunez_web/live/artists/show_live.ex
@@ -11,7 +11,8 @@ defmodule TunezWeb.Artists.ShowLive do
     artist =
       Tunez.Music.get_artist_by_id!(
         artist_id,
-        load: [:albums]
+        load: [:albums],
+        actor: socket.assigns.current_user
       )
 
     socket =
@@ -148,7 +149,10 @@ defmodule TunezWeb.Artists.ShowLive do
   end
 
   def handle_event("destroy-artist", _params, socket) do
-    case Tunez.Music.destroy_artist(socket.assigns.artist) do
+    case Tunez.Music.destroy_artist(
+           socket.assigns.artist,
+           actor: socket.assigns.current_user
+         ) do
       :ok ->
         socket =
           socket |> put_flash(:info, "Artist deleted successfully") |> push_navigate(to: ~p"/")
@@ -166,7 +170,10 @@ defmodule TunezWeb.Artists.ShowLive do
   end
 
   def handle_event("destroy-album", %{"id" => album_id}, socket) do
-    case Tunez.Music.destroy_album(album_id) do
+    case Tunez.Music.destroy_album(
+           album_id,
+           actor: socket.assigns.current_user
+         ) do
       :ok ->
         socket =
           socket
