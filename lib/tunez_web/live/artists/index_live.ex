a2783923e821829700bf8c8c7896d24c5f31b9ed
Leon Qadirie <leon.qadirie@nulltree.xyz>
2025-07-06 23:11:49 +0200

    feat: hide calls to non-authorized actions

--- lib/tunez_web/live/artists/index_live.ex
+++ lib/tunez_web/live/artists/index_live.ex
@@ -42,10 +42,8 @@ defmodule TunezWeb.Artists.IndexLive do
         <:action>
           <.search_box query={@query_text} method="get" data-role="artist-search" phx-submit="search" />
         </:action>
-        <:action>
-          <.button_link navigate={~p"/artists/new"} kind="primary">
-            New Artist
-          </.button_link>
+        <:action :if={Tunez.Music.can_create_artist?(@current_user)}>
+          <.button_link navigate={~p"/artists/new"} kind="primary">New Artist</.button_link>
         </:action>
       </.header>
 
