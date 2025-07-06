927c057deec4afce0850748219b839d2df7f5206
Leon Qadirie <leon.qadirie@nulltree.xyz>
2025-07-06 17:38:00 +0200

    feat: include Album data in Artist json api via different mechanism
    This will add a related relationship link to any artist API response, like http://localhost:4000/api/json/artists/[id]/albums. Accessing this related URL will then provide a list of the related albums in JSON format.

--- lib/tunez/music.ex
+++ lib/tunez/music.ex
@@ -11,6 +11,8 @@ defmodule Tunez.Music do
         post :create
         patch :update
         delete :destroy
+
+        related :albums, :read, primary?: true
       end
 
       base_route "/albums", Tunez.Music.Album do
