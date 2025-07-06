a9298a4b235e2cdb65769e3958b738a88ca6397b
Leon Qadirie <leon.qadirie@nulltree.xyz>
2025-07-06 21:41:30 +0200

    feat: expose Account via graphql

--- lib/tunez/accounts.ex
+++ lib/tunez/accounts.ex
@@ -1,6 +1,19 @@
 defmodule Tunez.Accounts do
   use Ash.Domain, otp_app: :tunez, extensions: [AshGraphql.Domain, AshJsonApi.Domain]
 
+  graphql do
+    mutations do
+      create Tunez.Accounts.User, :register_user, :register_with_password
+    end
+
+    queries do
+      get Tunez.Accounts.User, :sign_in_user, :sign_in_with_password do
+        identity false
+        type_name :user_with_token
+      end
+    end
+  end
+
   json_api do
     routes do
       base_route "/users", Tunez.Accounts.User do
