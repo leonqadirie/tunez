706f61ac2190abd99e419ca9885b0a0380d2500d
Leon Qadirie <leon.qadirie@nulltree.xyz>
2025-07-06 16:29:28 +0200

    feat: add Album aggregates to Artist

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -62,4 +62,10 @@ defmodule Tunez.Music.Artist do
       sort year_released: :desc
     end
   end
+
+  aggregates do
+    count :album_count, :albums
+    first :latest_album_year_released, :albums, :year_released
+    first :cover_image_url, :albums, :cover_image_url
+  end
 end
