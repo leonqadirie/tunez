d6de03cc15ff6c74d69bffa881e3dc896721c1c2
leonqadirie <leon.qadirie@nulltree.xyz>
2025-07-27 15:38:44 +0200

    feat: add relationship management between Album and Track

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -34,10 +34,16 @@ defmodule Tunez.Music.Album do
 
     create :create do
       accept [:name, :year_released, :cover_image_url, :artist_id]
+      argument :tracks, {:array, :map}
+      change manage_relationship(:tracks, type: :direct_control)
     end
 
     update :update do
       accept [:name, :year_released, :cover_image_url]
+      # Ash might allow this to be atomic in the future
+      require_atomic? false
+      argument :tracks, {:array, :map}
+      change manage_relationship(:tracks, type: :direct_control)
     end
   end
 
