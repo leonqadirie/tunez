4d6ff677a8914fa57c0ffb2c0e2894db4af88da7
leonqadirie <leon.qadirie@nulltree.xyz>
2025-07-27 20:58:31 +0200

    feat: follow Artists (if logged in)
    feat: follow Artists

--- lib/tunez_web/live/artists/show_live.ex
+++ lib/tunez_web/live/artists/show_live.ex
@@ -11,7 +11,7 @@ defmodule TunezWeb.Artists.ShowLive do
     artist =
       Tunez.Music.get_artist_by_id!(
         artist_id,
-        load: [albums: [:duration, :tracks]],
+        load: [:followed_by_me, albums: [:duration, :tracks]],
         actor: socket.assigns.current_user
       )
 
@@ -29,6 +29,10 @@ defmodule TunezWeb.Artists.ShowLive do
       <.header>
         <.h1>
           {@artist.name}
+          <.follow_toggle
+            :if={Tunez.Music.can_follow_artist?(@current_user, @artist)}
+            on={@artist.followed_by_me}
+          />
         </.h1>
         <:subtitle :if={
           @artist.previous_names !=
@@ -203,6 +207,15 @@ defmodule TunezWeb.Artists.ShowLive do
   end
 
   def handle_event("follow", _params, socket) do
+    socket =
+      case Tunez.Music.follow_artist(socket.assigns.artist, actor: socket.assigns.current_user) do
+        {:ok, _} ->
+          update(socket, :artist, &%{&1 | followed_by_me: true})
+
+        {:error, _} ->
+          put_flash(socket, :error, "Could not follow artist #{socket.assigns.artist.name}")
+      end
+
     {:noreply, socket}
   end
 
