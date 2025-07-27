2f189f3d03c560f9fac70d3f34d0d8ea66e24e06
leonqadirie <leon.qadirie@nulltree.xyz>
2025-07-27 17:52:59 +0200

    feat: add ArtistFollower policy

--- lib/tunez/music/artist_follower.ex
+++ lib/tunez/music/artist_follower.ex
@@ -1,5 +1,9 @@
 defmodule Tunez.Music.ArtistFollower do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    authorizers: [Ash.Policy.Authorizer]
 
   postgres do
     table "artist_followers"
@@ -13,6 +17,12 @@ defmodule Tunez.Music.ArtistFollower do
 
   actions do
     defaults [:read]
+  end
+
+  policies do
+    policy action_type(:read) do
+      authorize_if always()
+    end
   end
 
   relationships do
