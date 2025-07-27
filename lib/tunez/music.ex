0f64b148cea171f5e01c11032ea4444814ab17ae
leonqadirie <leon.qadirie@nulltree.xyz>
2025-07-27 20:35:59 +0200

    feat: extend ArtistFollower with graphql

--- lib/tunez/music.ex
+++ lib/tunez/music.ex
@@ -17,6 +17,8 @@ defmodule Tunez.Music do
       create Tunez.Music.Album, :create_album, :create
       update Tunez.Music.Album, :update_album, :update
       destroy Tunez.Music.Album, :destroy_album, :destroy
+
+      create Tunez.Music.ArtistFollower, :follow_artist, :create
     end
   end
 
